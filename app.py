2437d6710f4a00747de26301348f1d791a5d2888
Morris Hazrati <87606793+manijehshirzadeh@users.noreply.github.com>
2024-07-26 13:39:42 +1000

    added print_message, render and get_move

--- app.py
+++ app.py
@@ -19,6 +19,36 @@ class Game:
     3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
   """)
 
+  def print_message(self):
+    ## If there is a tie: print("Tie game!")
+    ## If there is a winner: print(f"{self.winner} wins the game!")
+    ## Otherwise: print(f"It's player {self.turn}'s turn!")
+    print(f'{self.turn} turn')
+
+  def render(self):
+    # Call upon print_board
+    self.print_board()
+    ## Call upon print_message
+    self.print_message()
+    
+  def get_move(self):
+    while True:
+      # prompt user for input
+      move = input(f"Enter a valid movie (example: A1): ").lower()
+      
+      # If the input is valid, update the board and break the loop
+      if move == 'a1' or move == 'a2' or move == 'a3' or move == 'b1' or move == 'b2' or move == 'b3' or move == 'c1' or move == 'c2' or move == 'c3':
+        print(move)
+        print(self.board[move])
+        if self.board[move] == None:
+          self.board[move] = self.turn
+          break
+        else: 
+          # otherwise, print a message notifying the user of the invalid input and allow the loop to continue
+          print("This cell is already occupied, select another cell.")
+      else:
+        print("invalid input")
+
 game_instance = Game("X", False, None, {
   'a1': None, 'b1': None, 'c1': None,
   'a2': None, 'b2': None, 'c2': None,
@@ -26,4 +56,5 @@ game_instance = Game("X", False, None, {
 } )
 
 game_instance.play_game()
-game_instance.print_board()
+game_instance.render()
+game_instance.get_move()
